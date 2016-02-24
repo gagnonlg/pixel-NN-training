5fa88d2efc788839856d2393328d94662c13e908
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2016-02-24 15:19:26 -0500

    gensql: correct variables to feed to residuals.cxx

--- gensql.py
+++ gensql.py
@@ -89,8 +89,10 @@ def sql_position(nparticles, sizeY):
     for i in range(sizeY):
         selected.append('NN_pitches%d' % i)
     for i in range(nparticles):
-        selected.append('NN_position_id_X_%d' % i)
-        selected.append('NN_position_id_Y_%d' % i)
+        selected.append('NN_position_id_X_%d_TRUTH' % i)
+        selected.append('NN_position_id_X_%d_PRED' % i)
+        selected.append('NN_position_id_X_%d_TRUTH' % i)
+        selected.append('NN_position_id_X_%d_PRED' % i)
 
     for lname,l,b in layers:
         name = "residuals_%s" % lname
