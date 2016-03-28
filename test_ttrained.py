15873b73b46f892200903ed1ee861b212d20ffa7
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2016-03-28 17:57:01 -0400

    add weights equality check for keras to/from ttrained conversion test

--- test_ttrained.py
+++ test_ttrained.py
@@ -15,3 +15,10 @@ mo2,no2 = ttrained.to_keras(tt2)
 
 assert mo2.to_json() == mo.to_json(), 'model equality'
 assert np.array_equal(no['std'] ,no2['std']) and np.array_equal(no['mean'] ,no2['mean']), 'normalization equality'
+
+def weights(m):
+    return [l.get_weights() for l in m.layers if type(l) == 'Dense']
+
+for (w1, b1), (w2, b2) in zip(weights(mo),weights(mo2)):
+    assert np.array_equal(w1,w2)
+    assert np.array_equal(w2,b2)
